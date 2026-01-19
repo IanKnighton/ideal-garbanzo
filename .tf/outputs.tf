2248d4a44761c5ac8d9be5f85e617a4fb4841b5d
Ian Knighton <ian@knighton.dev>
2026-01-19 15:30:33 -0700

    chore: cleanup variables

---
+++ .tf/outputs.tf
@@ -0,0 +1,4 @@
+output "external_ip" {
+  description = "The external IP address of the Kubernetes service"
+  value       = kubernetes_service.service.status[0].load_balancer[0].ingress[0].ip
+}
--- .tf/variables.tf
+++ .tf/variables.tf
@@ -1,10 +1,9 @@
 variable "default_region" {
   description = "The default region for cloud resources"
   type        = string
-  default     = "us-west3" # Replace with your preferred GCP region
 }
 
-# In private repos, you can hardcode this. 
+# In private repos, you can hardcode this (probably). 
 # In public repos, use GitHub Actions secrets to set this value.
 variable "default_project" {
   description = "The default GCP project ID"
@@ -20,13 +19,11 @@ variable "repository_owner" {
 variable "repository_name" {
   description = "The name of the GitHub repository"
   type        = string
-  default     = "ideal-garbanzo" # Replace with your GitHub repository name
 }
 
 variable "container_image" {
   description = "The container image to deploy"
   type        = string
-  default     = "us-docker.pkg.dev/jump-test-ik/ideal-garbanzo/app:20260119.3" # Replace with your desired container image
 }
 
 variable "instance_name" {
